b2ed2b8b0ca2d2d60849fe8b44b27c1accbd9a68
Babi Rai <babi.rai@hotmail.com>
2024-07-31 21:01:19 +0100

    Dynamically set footer to current year

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript is running...');
+});
+
+
+// Set the current year in the footer
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('current-year').textContent = new Date().getFullYear();
 });
